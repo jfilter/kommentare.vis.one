cb4955ba3be0c5808f4d0e1c842c503c3a7de852
Johannes Filter <hi@jfilter.de>
2020-02-04 20:00:06 +0100

    improving typeahead, texts, etc

--- components/data-loader.js
+++ components/data-loader.js
@@ -3,40 +3,37 @@ const d3 = require("d3");
 
 class DataLoader extends D3Component {
   loadData(props) {
-    if (
-      !"value" in props ||
-      !"prevSrc" in props.value ||
-      props.value.prevSrc != props.src
-    ) {
-      d3.json(props.src).then(parsed => {
-        const { tokens } = parsed;
-        let data = {};
-        if ("vectors" in parsed) {
-          data = parsed.vectors.map((xx, i) => {
-            return { x: xx[0], y: xx[1] };
-          });
-        }
-
-        let sims = {};
-        if ("sims" in parsed) {
-          sims = parsed.sims;
-        }
-
-        props.updateProps({
-          value: { data, tokens, sims, prevSrc: props.src }
+    d3.json(props.src).then(parsed => {
+      const { tokens } = parsed;
+      let data = {};
+      if ("vectors" in parsed) {
+        data = parsed.vectors.map((xx, i) => {
+          return { x: xx[0], y: xx[1] };
         });
+      }
+
+      let sims = {};
+      if ("sims" in parsed) {
+        sims = parsed.sims;
+      }
+
+      props.updateProps({
+        value: { data, tokens, sims }
       });
-    } else {
-      props.updateProps({ load: false });
-    }
+    });
   }
 
   initialize(node, props) {
-    this.loadData(props);
+    this.currentSrc = "";
   }
 
   update(props, oldProps) {
-    this.loadData(props);
+    this.currentSrc = props.src;
+    if (props.src !== oldProps.src) {
+      setTimeout(() => {
+        if (props.src === this.currentSrc) this.loadData(props);
+      }, props.timeout);
+    }
   }
 }
 
