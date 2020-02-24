5e36a6a4b97e44dd21006e079196825e469aeed2
Johannes Filter <hi@jfilter.de>
2020-02-25 00:41:08 +0100

    dont show video for null

--- components/video.js
+++ components/video.js
@@ -5,26 +5,28 @@ class Video extends React.PureComponent {
     const { q } = this.props;
     return (
       <div className="video-container">
-        <video
-          key={q}
-          controls
-          autoPlay
-          loop
-          playsInline
-          width="100%"
-          height="100%"
-        >
-          <source
-            src={`http://kommentare.vis.one/videos/${q}.mp4`}
-            type="video/mp4"
-          />
-          <source
-            src={`http://kommentare.vis.one/videos/404.mp4`}
-            type="video/mp4"
-          />
-          Bitte einen anderen Browser, z. B. Firefox oder Chrome, benutzen. Das
-          Video kann nicht angezeigt werden.
-        </video>
+        {q && (
+          <video
+            key={q}
+            controls
+            autoPlay
+            loop
+            playsInline
+            width="100%"
+            height="100%"
+          >
+            <source
+              src={`http://kommentare.vis.one/videos/${q}.mp4`}
+              type="video/mp4"
+            />
+            <source
+              src={`http://kommentare.vis.one/videos/404.mp4`}
+              type="video/mp4"
+            />
+            Bitte einen anderen Browser, z. B. Firefox oder Chrome, benutzen.
+            Das Video kann nicht angezeigt werden.
+          </video>
+        )}
       </div>
     );
   }
