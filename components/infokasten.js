77ea34a7490e10df19909cc608cad542e4035e0b
Johannes Filter <hi@jfilter.de>
2020-04-13 00:25:58 +0200

    improve first page

--- components/infokasten.js
+++ components/infokasten.js
@@ -7,12 +7,12 @@ class Infokasten extends React.PureComponent {
         <h3>Strukturen abbilden</h3>
         <p>
           Dies ist eine Visualisierung von einem Word Embedding. Das bedeutet,
-          hier werden die Beziehungen zwischen den Worten
-          gezeigt,runtergebrochen auf zwei Dimensionen. In der Mitte steht ein
-          Wort, darum herum angeordnet sind die zehn ähnlichsten Wörter
-          angeordnet. Je ähnlicher sich die Begriffe sind, desto kürzer sind die
-          Distanzen zueinander. Begriffe die sich unähnlich sind, werden visuell
-          weiter entfernt voneinander dargestellt. Die Achsen haben hier keine
+          hier werden die Beziehungen zwischen den Worten gezeigt,
+          runtergebrochen auf zwei Dimensionen. In der Mitte steht ein Wort,
+          darum herum angeordnet sind die zehn ähnlichsten Wörter angeordnet. Je
+          ähnlicher sich die Begriffe sind, desto kürzer sind die Distanzen
+          zueinander. Begriffe die sich unähnlich sind, werden visuell weiter
+          entfernt voneinander dargestellt. Die Achsen haben hier keine
           Bedeutung. [Hier erklären wir, wie genau es eine Ebene abgebildet
           werden kann.](/hintergrund/#todo)
         </p>
--- components/overview1.js
+++ components/overview1.js
@@ -1,17 +1,56 @@
 const React = require("react");
+const Video = require("./video");
 
 class Overview1 extends React.PureComponent {
   render() {
     return (
       <div className="infokasten">
-        <h3>Übersicht</h3>
+        <h3>Übersicht über das Projekt</h3>
         <p>
-          Das Projekt besteht aus zwei Teilen. Auf dieser Seite werden
-          Grundlagen von Machine Learning eklärt.{" "}
-          <a href="/zeit/">Im zweiten Teil</a> geht es darum, Machine Learning
-          anzuwenden, um Veränderung nachzuvollziehen. Das Projekt ist im Rahmen
-          des <a href="https://prototypefund.de/">Prototype Fund</a> enstanden
-          und wurde vom <a href="https://www.bmbf.de/">BMBF</a> finanziert.
+          Im ersten Teil (diese Seite) werden Grundlagen von Machine Learning
+          eklärt. <a href="/zeit/">Im zweiten Teil</a> wird mithilfe von Machine
+          Learning die Veränderung der Sprache in den Kommentarenspalte
+          visualisiert.
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              alignContent: "center",
+            }}
+          >
+            <video
+              controls
+              autoPlay
+              loop
+              playsInline
+              width="60%"
+              height="60%"
+              style={{ margin: "1rem auto" }}
+            >
+              <source
+                src={`http://kommentare.vis.one/videos/mittelmeer.mp4`}
+                type="video/mp4"
+              />
+              <source
+                src={`http://kommentare.vis.one/videos/404.mp4`}
+                type="video/mp4"
+              />
+              Bitte einen anderen Browser, z. B. Firefox oder Chrome, benutzen.
+              Das Video kann nicht angezeigt werden.
+            </video>
+            <div>
+              <a href="/zeit/">👉 zu Teil II springen</a>
+            </div>
+          </div>
+          <br></br>
+          Das Projekt ist im Rahmen des{" "}
+          <a href="https://prototypefund.de/">Prototype Fund</a> enstanden und
+          wurde vom{" "}
+          <a href="https://www.bmbf.de/">
+            Bundesministerium für Bildung und Forschung
+          </a>{" "}
+          finanziert.
         </p>
       </div>
     );
